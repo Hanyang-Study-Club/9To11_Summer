73538220deea933590a36212a2db9e9647a1249f
chocchic <60125582+chocchic@users.noreply.github.com>
2023-08-04 23:14:30 +0900

    3주차 chocchic

--- 3주차_힙_정렬/005_LV2_가장_큰_수/chocchic.java
+++ 3주차_힙_정렬/005_LV2_가장_큰_수/chocchic.java
@@ -8,7 +8,7 @@ class Solution {
             nums[i] = numbers[i]+"";
         }
         
-        if(Integer.parseInt(nums[0]) == 0 && Integer.parseInt(nums[numbers.length -1]) == 0) return "0";
+        if(Integer.parseInt(nums[0]) == 0 && Integer.parseInt(nums[numbers.length -1]) == 0) return "0"; // 0 00 00 00 000 이런 식으로 0만 있으면 그냥 0임
         
         Arrays.sort(nums, new Comparator<String>(){
             @Override
