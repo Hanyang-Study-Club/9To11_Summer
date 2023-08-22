fbac5f6d5d7bf0235b3bb06abd1cc764065abddf
chocchic <60125582+chocchic@users.noreply.github.com>
2023-08-22 22:08:50 +0900

    Create chocchic.java

---
+++ 5주차_탐욕법/005_LV3_섬_연결하기/chocchic.java
@@ -0,0 +1,40 @@
+// 크루스칼 알고리즘
+import java.util.*;
+
+class Solution {
+    int parent[];
+    
+    public int find(int node){
+        if(parent[node] == node) return node;
+    
+        return find(parent[node]);
+    }
+    
+    public void union(int a, int b){
+        a = find(a);
+        b = find(b);
+        
+       if (a > b) parent[a] = b;
+        else parent[b] = a;
+    }
+    
+    public int solution(int n, int[][] costs) {
+        int answer = 0;
+        
+        int size = costs.length;
+        Arrays.sort(costs, (o1, o2) -> Integer.compare(o1[2], o2[2]));
+       
+        parent = new int[n];
+        for (int i = 0; i < n; i++) {
+			parent[i] = i;
+		}
+        for(int i = 0; i < size; i++){
+            if(find(costs[i][0]) != find(costs[i][1])){
+                union(costs[i][0], costs[i][1]);
+                answer+=costs[i][2];
+            }
+        }
+        
+        return answer;
+    }
+}
